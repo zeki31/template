105d6e0cbb211a32fed044a6d988c9aee187edcd
zeki31 <ozeki.f.h.13@gmail.com>
2025-04-21 11:00:51 +0900

    first commit

--- dependencies.sh
+++ dependencies.sh
@@ -1,11 +1,10 @@
-uv init --name template --package --python 3.10
-uv add install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 
+uv sync
+uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 
+MAX_JOBS=4 uv pip install 'git+https://github.com/facebookresearch/pytorch3d.git' --no-build-isolation
+uv pip install -U xformers==0.0.27.post2 --index-url https://download.pytorch.org/whl/cu118
+
 uv add install -r requirements.txt
 
-uv add install --dev pre-commit
-
-# For PyTorch3D
-MAX_JOBS=4 uv add install 'git+https://github.com/facebookresearch/pytorch3d.git' --no-build-isolation
-
-# For submodules
 uv add install -e . --no-build-isolation
+
+uv add install --dev pre-commit
--- init_uv.sh
+++ init_uv.sh
@@ -1,3 +1,1 @@
-uv python install 3.10
-uv python pin 3.10
 uv venv
